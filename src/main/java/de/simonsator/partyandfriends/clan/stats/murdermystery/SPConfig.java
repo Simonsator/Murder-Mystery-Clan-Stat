a90929779bd17d79306da342711c70e65c66a46c
Simonsator <00pflaume@gmail.com>
2023-04-08 14:22:36 +0200

    Initial upload

---
+++ src/main/java/de/simonsator/partyandfriends/clan/stats/murdermystery/SPConfig.java
@@ -0,0 +1,27 @@
+package de.simonsator.partyandfriends.clan.stats.murdermystery;
+
+
+import de.simonsator.partyandfriends.api.PAFExtension;
+import de.simonsator.partyandfriends.utilities.ConfigurationCreator;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SPConfig extends ConfigurationCreator {
+
+	protected SPConfig(File file, PAFExtension pPlugin) throws IOException {
+		super(file, pPlugin);
+		readFile();
+		loadDefaultValues();
+		saveFile();
+	}
+
+	private void loadDefaultValues() {
+		set("database.host", "localhost");
+		set("database.port", 3306);
+		set("database.db", "villager");
+		set("database.user", "root");
+		set("database.password", "Password");
+		set("database.ssl", false);
+	}
+}
